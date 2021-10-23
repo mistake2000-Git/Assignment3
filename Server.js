12f94b71cb095d6dfaa84301cbe9197496194bf8
mistake2000-Git <18521002@gm.uit.edu.vn>
2021-10-23 22:04:03 +0700

    Get request

--- Server.js
+++ Server.js
@@ -5,24 +5,29 @@ let BodyParser = require('body-parser');
 let MethodOverride = require('method-override');
 const path=require('path');
 const { application } = require('express');
+const { parse } = require('querystring');
 
 // our components
 app.listen(3000,()=>console.log('Server running in port 3000!'));
 
+const cities = [
+  {id:1, name:"Ben Tre"},
+  {id:2 ,name:" Sai Gon"}
+];
 
 /*app.get('/',(req,res)=>{
     res.send('Hello World');
 });*/
-app.use(express.static(__dirname))
 app.get('/',(req,res)=>{
-  res.sendFile(__dirname+'/views/index.html');
+  res.send("Hello World");
 })
 
-app.get("/json", (req,res)=>{ 
-      res.json({
-        message:"Hello json"
-      });
-  })
-app.get("/",(req,res)=>{
-  res.send('Got a POST request');
+
+app.get('/api',(req,res)=>{
+  res.send([1,2,3]);  
+})
+
+app.get('/api/:id',(req,res)=>{
+  const city = cities.find(c=>c.id ===parseInt(req.params.id));
+  res.send(city)  ;
 })
