9a6a5c0dde2202a4cd9eb27a1a13ffb0b162ce85
mistake2000-Git <18521002@gm.uit.edu.vn>
2021-10-26 19:44:08 +0700

    document

--- Server.js
+++ Server.js
@@ -87,3 +87,27 @@ app.delete('/api/',(req,res)=>{
     res.send(cities);
   }
 })
+
+
+
+
+/**
+ * @api {get} /api Request all City
+ * @apiName GetCity
+ * @apiGroup City
+ *
+ * @apiSuccess {String} ID id of the City.
+ * @apiSuccess {String} Name name  of the city.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *    [
+            {id:1, name:"Ben Tre"}, 
+            {id:2 ,name:"Ho Chi Minh"},
+            {id:3 ,name:"Da Nang"},
+            {id:4 ,name:"Ha Noi"},
+            {id:5 ,name:"Tien Giang"}
+        ]
+ *T
+ * @apiSampleRequest http://localhost:3000/api
+ */
